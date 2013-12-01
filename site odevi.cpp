ce6b1b36fdb9c6446fa092ca55d4eaaa36b0553e
oguzAslAn <oguzaslanxx@hotmail.com>
2013-12-01 23:26:54 +0200

    site odevi.cpp

---
+++ site odevi.cpp
@@ -0,0 +1,210 @@
+<!DOCTYPE html>
+<html>
+<body style="background-color:#FFFFFF"   background="red.jpg">
+
+<table width="100%">
+<tr>
+<th ></th>
+<th ></th>
+</tr>
+<tr>
+<td>
+
+
+
+
+
+
+<meta charset="UTF-8" />
+<a href="#"><img src="fotolar/spor.jpg"  border="0"></a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<h2><font color="#3333CC">SPOR YAPMANIN 10 FAYDASI </Font></h2>
+<h4><font color="#3333CC">1. Bunama Riskini Azaltır.</Font> </h4> <p>Birçok araştırma egzersizin zihin fonksiyonlarını geliştirdiğini, 
+enerjiyi artırdığını böylece de bunama riskini düşürdüğünü gösteriyor. Amerika Alzheimer Birliği&#8217;ne 
+göre &#8220;fiziksel aktivite sağlıklı insanlarda bilişsel fonksiyonları geliştiriyor ve zihinsel 
+bozuklukların önüne geçiyor.&#8221; Özellikle yeni bir spora başlamak hafıza ve konsantrasyon gibi 
+öğrenme yetenekleri istediği için daha faydalı.</p>
+<h4><font color="#3333CC">2. Kemik Erimesi (Osteoporoz) Riskini Düşürür</Font></h4> <p>Fitness, özellikle ağırlık antrenmanları kemik 
+sağlığı için önemlidir. Egzersiz osteoporozdan korunmak ve kemik kitlesini sabit tutmak için yapacağınız en 
+önemli şeylerden biri. Arizona Üniversitesi&#8217;ne göre ağırlık kaldırma, hafif tempolu koşma, merdiven 
+çıkmak, step aerobik, dans ve raket sporları bu konuda en iyileri.</p> <h4><font color="#3333CC">3. Kas Kaybını Engeller</Font></h4> 
+<p>The American Journal of Clinical Nutrition&#8217;da yayınlanan bir araştırmaya göre, yaşlandıkça hem 
+vücudun kas üretme kapasitesi düşer, hem de sahip olduğumuz kaslar daha çabuk azalır. Bu yüzden sağlıklı 
+bir yaşlanma için düzenli egzersiz çok önemlidir. Egzersiz kas kitlesini korumaya yardım etmekle beraber, 
+kas oluşumunu tetikler. Böylece metabolizmamız daha hızlı çalışır, günlük işlerimizi bitirmemiz için daha 
+güçlü ve dayanıklıklı oluruz.</p> <h4><font color="#3333CC">4. Cinsel Gücü Artırır</Font></h4> <p>Egzersiz bir mutluluk hormonu olan 
+endorfin salgısını tetikler. Cinsel güç ve istek de endorfin sistemini kullanır. Egzersiz yapıldığında 
+vücudunuz endorfin salgılar. Endorfin salgısı ne kadar sık ve güçlü olursa, cinsel haz da o kadar artar.
+</p> <h4><font color="#3333CC">5. Sindirimi Kolaylaştırır</Font></h4> <p>Amerika Gastroenteroloji Birliği araştırmasına göre egzersiz 
+abdominal kasları güçlendirerek, bağırsak kaslarının gıdaları daha kolay parçalamasını ve sindirim sistemi 
+boyunca bunların daha kolay geçmesini sağlıyor. Yale Üniversitesi&#8217;nden Dr. Katz bu sonuçlara ek 
+olarak kabızlığın da spor yaparak engeleneceğini söylüyor. Gün boyunca ara sıra yapılan yürüyüşlerin bile 
+gözle görülür ölçüde yararlı olacağını belirtiyor.</p> <h4><font color="#3333CC">6. Stres, Depresyon ve Anksiyeteyi Azaltır</Font></h4>
+ <p>Yıllardır fiziksel aktivitenin ruh halini pozitif yaptığını duyarız ama bundan da fazlası var. Mayo 
+ Klinik&#8217;e göre &#8220;bir kere motive olduğunuzda, egzersiz büyük bir fark yaratabilir. Egzersiz 
+ yapmak rahatlamanızı, daha iyi hissetmenizi, depresyon ve anksiyeteden korunmanızı sağlar.&#8221; 
+ Peki nasıl? Egzersiz yaptığınızda depresyonu azaltan nörotransmiterler ve endorfin salgılanır. Ayrıca 
+ vücut sıcaklığınız yükselir, bu da sinirleri yatıştırır.</p> <h4><font color="#3333CC">7. Mental Performansı ve İş 
+ verimliliğini Artırır</Font></h4> <p>Dr. Katz spor yapmanın hayat kalitesini yükselteceğini özellikle iş 
+ verimliliğini artıracağını söylüyor. Egzersiz hem özgüveni yükselterek, liderlik almanızı sağlar, hem de 
+ verimliliği ve odaklanmayı artırır. Egzersiz tansiyonu, stresi ve mental yorgunluğu azaltır, genel 
+ anlamda enerji verir, uyku kalitelileşir. Böylece mental keskinlik de artmış olur.</p> <h4><font color="#3333CC">8. Kanser 
+ Riskini Azaltır</Font></h4> <p>Birçok araştırma akciğer, kolon ve meme kanseri risklerinin düzenli egzersiz 
+ sayesinde önemli ölçüde azaldığını gösteriyor. Egzersiz kadınlarda östrojen hormonunu azaltarak meme 
+ kanseri riskini düşürüyor.</p> <h4><font color="#3333CC">9. Kalp Hastalıkları Riskini Azaltır</Font></h4> <p>Egzersiz yüksek tansiyon,
+ yüksek kolesterol ve diyabeti önlediği için kalp problemleri de otomatik olarak azalıyor. Ayrıca egzersiz
+ yapmayanlarda kalp krizi daha ağır oluyor.</p> <h4><font color="#3333CC">10. Cildi Güzelleştirir</Font></h4> <p>Spor yaparken ilk 
+ amacınız iyi görünmekse, cilt sağlığını da motivasyonlarınıza ekleyin. Dr. Katz &#8220;egzersizin cilde 
+ giden kan akışını artırdığını&#8221; belirtiyor. Araştırmalar ayrıca egzersizin DHEA ve DHT gibi 
+ testesteron hormonlarının üretimini kontrol altına aldığını, böylece de akne oluşumunu azalttığını 
+ gösteriyor. Ayrıca terlemek de gözenekleri açarak, cildin yağlardan ve kirden arınmasını sağlıyor.</p>
+ <p><font color="#3333CC">SPOR DALLARINA BAKMAK İÇİN LÜTFEN TIKLAYINIZ : <a href="http://tr.wikipedia.org/wiki/Spor">vikipedia girmek için lütfen tıklayınız</a></p>
+ 
+ <img src="fotolar/raf.jpg"  border="0">
+ 
+ 
+ <p><font color="#3333CC">EN ÖNEMLİ 5 SPOR DALI : </p>
+ 
+ <p><font color="#3333CC">1-Futbol : <a href="http://www.youtube.com/watch?v=8qQEZZp0XVQ">bilgi sahibi olmak için lütfen tıklayınız</a></p>
+ <p><font color="#3333CC">2-Tenis : <a href="http://www.youtube.com/watch?v=OCt6SpNYa4U">bilgi sahibi olmak için lütfen tıklayınız</a></p>
+ <p><font color="#3333CC">3-Kriket : <a href="http://www.youtube.com/watch?v=AlLXJUJm1lc">bilgi sahibi olmak için lütfen tıklayınız</a></p>
+ <p><font color="#3333CC">4-Beyzbol : <a href="http://www.youtube.com/watch?v=Wf8nt4Tv76k">bilgi sahibi olmak için lütfen tıklayınız</a></p>
+ <p><font color="#3333CC">5-Masa Tenisi :<a href="http://www.youtube.com/watch?v=ktmxD5J93DU">bilgi sahibi olmak için lütfen tıklayınız</a></p>
+
+
+   
+ 
+ 
+ </td>
+<td>
+<p><center><embed allowscriptaccess="never" pluginspage="http://www.macromedia.com/go/getflashplayer" src="http://img135.imageshack.us/img135/5596/hlclockfj3.swf" width="160" height="160" wmode="transparent" type="application/x-shockwave-flash" quality="high"></embed> <noscript><a href="http://html-java-kod-bul.tr.gg">html kodları</center></a></noscript></p>
+
+<div align="center"><a href="http://canli-radyo-dinle.net/" target="_blank" title="Canli radyo dinlemek icin tiklayiniz.."><img src="http://canli-radyo-dinle.net/images/radyodinle.png" width="120" height="120" alt="radyo dinle" /></a></div>
+<img src="fotolar/a.jpg"  border="0">
+<img src="fotolar/as.jpg"  border="0">
+<img src="fotolar/re.jpg"  border="0">
+<img src="fotolar/tr.jpg"  border="0">
+<img src="fotolar/golf.jpg"  border="0">
+<img src="fotolar/pl.jpg"  border="0">
+
+<div style="text-align:center"> 
+  <button onclick="playPause()">Play/Pause</button> 
+  <button onclick="makeBig()">Big</button>
+  <button onclick="makeSmall()">Small</button>
+  <button onclick="makeNormal()">Normal</button>
+  <br> 
+  <video id="video1" width="420">
+    <source src="sabah.webm" type="video/mp4">
+    <source src="mov_bbb.ogg" type="video/ogg">
+    Your browser does not support HTML5 video.
+  </video>
+</div> 
+
+<script> 
+var myVideo=document.getElementById("video1"); 
+
+function playPause()
+{ 
+if (myVideo.paused) 
+  myVideo.play(); 
+else 
+  myVideo.pause(); 
+} 
+
+function makeBig()
+{ 
+myVideo.width=560; 
+} 
+
+function makeSmall()
+{ 
+myVideo.width=320; 
+} 
+
+function makeNormal()
+{ 
+myVideo.width=420; 
+} 
+</script> 
+</td>
+</tr>
+
+</table>
+<center>
+ <table id="table3">
+
+    <tr>
+
+     <td colSpan="20" align="left"><b>Bu sayfa hakkında yorum 
+
+     göndermek için:</b> 
+
+     </td>
+
+    </tr>
+
+    <tr>
+
+     <td align="left">İsim
+
+     </td>
+
+     <td align="left"><input size="25" name="isim"> 
+
+     </td>
+
+    </tr>
+
+    <tr>
+
+     <td align="left">E-mail
+
+     </td>
+
+     <td align="left">
+
+     <input size="25" name="mail" AUTOCOMPLETE="OFF"> 
+
+     </td>
+
+    </tr>
+
+    <tr>
+
+     <td>Mesajınız:</td>
+
+     <td>
+
+     <textarea style="WIDTH: 250px; HEIGHT: 100px" name="icerik" rows="1" cols="20"></textarea></td>
+
+    </tr>
+
+   </table>
+
+   <input value="http://www.verdailesohbet.com//wsx_yorum_sonuc.html" type="hidden" name="page">
+
+   <input value="verdailesohbet.com" type="hidden" name="domain">
+
+   <input value="30842" type="hidden" name="id">
+
+   <input value="0" type="hidden" name="alt_id">
+
+   <input value="0" type="hidden" name="alt_id1">
+
+   <input value="index.htm" type="hidden" name="dosya_ismi">
+
+   <input value="gönder" type="submit"> 
+
+  </form>
+</center>
+  </center></center></td>
+
+ </tr>
+
+</table>
+ <p></p>
+ <a target='_blank' href='http://www.facebook.com/share.php?u=http://paylaşılacaksiteadresi.html'>Facebookta Paylaşmak için tıklayınız</a>&nbsp;&nbsp;&nbsp;
+<a href="https://twitter.com/share" target="_blank" data-url="http://paylaşılacaksiteadresi.html"  data-lang="tr" >Twittirda Paylaşmak için fütfen tıklayınız</a>
+ 
+</body>
+</html>
+
